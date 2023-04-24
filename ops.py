95d15b76d4c19ef91e964f8373c613b4987a860e
aartibh <73574700+aartibh@users.noreply.github.com>
2023-04-24 15:41:48 +0530

    Update ops.py

--- ops.py
+++ ops.py
@@ -1,11 +1,22 @@
-def add(x,y):
-    return x+y
+# def add(x,y):
+#     return x+y
 
-def subtract(x,y):
-    return x-y
+# def subtract(x,y):
+#     return x-y
 
-def multiply(x,y):
-    return x*y
+# def multiply(x,y):
+#     return x*y
 
-def divide(x,y):
-    return x/y
+# def divide(x,y):
+#     return x/y
+
+# Python3 program to add two numbers
+
+num1 = 15
+num2 = 12
+
+# Adding two nos
+sum = num1 + num2
+
+# printing values
+print("Sum of {0} and {1} is {2}" .format(num1, num2, sum))
